da7f91e3d19fd9a595bfe6e4c11a0df442527b03
Barao <adilso.perito@gmail.com>
2025-05-27 13:52:14 -0300

    Corrige blank lines em app.py para flake8

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 
-# ESTA FUNÇÃO DEVE ATENDER APENAS A ROTA "/"
 @app.route("/")
 def home_page():
     return render_template("index.html")
 
 
-# ESTA FUNÇÃO DEVE ATENDER APENAS A ROTA "/api/status"
 @app.route("/api/status")
 def api_status():
     return jsonify({
@@ -21,6 +19,3 @@ def api_status():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
