8e290719afe36ca46e6464f282c423a095fad8f7
Barao <adilso.perito@gmail.com>
2025-05-27 13:26:43 -0300

    Ajusta quebras de linha em app.py para flake8

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, jsonify
 
+
 app = Flask(__name__)
+
+
 # ESTA FUNÇÃO DEVE ATENDER APENAS A ROTA "/"
 @app.route("/")
 def home_page():
     return render_template("index.html")
+
+
 # ESTA FUNÇÃO DEVE ATENDER APENAS A ROTA "/api/status"
 @app.route("/api/status")
 def api_status():
@@ -12,6 +17,7 @@ def api_status():
         "status": "ok",
         "message": "Seu app Flask está vivo!"
     })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
